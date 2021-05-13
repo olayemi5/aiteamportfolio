79eb1b5d2d564a85d205bd69408b869c4567c841
Stephen Olayemi <stephen.olayemi@innotelligent.com>
2021-05-13 11:29:06 +0100

    First Push

---
+++ Portfolio/Portfolio/Controllers/CompanyController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Controllers
+{
+    public class CompanyController : Controller
+    {
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+        public IActionResult WhyChooseUs()
+        {
+            return View();
+        }
+        public IActionResult OurTeam()
+        {
+            return View();
+        }
+    }
+}
---
+++ Portfolio/Portfolio/Controllers/ServiceController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Controllers
+{
+    public class ServiceController : Controller
+    {
+        public IActionResult ITServices()
+        {
+            return View();
+        }
+    }
+}
